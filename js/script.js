8b30a3303413d7d0156be9f2003403d35b9d9f8f
halo2008 <ksedkowski@gmail.com>
2018-07-16 16:29:30 +0200

    update

--- js/script.js
+++ js/script.js
@@ -3,113 +3,93 @@
 
 var output = document.getElementById('output-temp');
 
-var output_2 = document.getElementById('output-text');
-
-var output_3 = document.getElementById('water');
+var outputText =document.getElementById('output-text');
 
 var button = document.getElementById('btn-cel');
 
-var button_2 = document.getElementById('btn-cel-2');
+var buttonFahrenheit = document.getElementById('btn-cel-2');
 
-var temp;
-
-var tempx;
+var waterState = document.getElementById('water');
 
 button.addEventListener('click', function(){  
     
-  temp = window.prompt('wpisz temperature w stopniach celciusza');
+  var temp = window.prompt('wpisz temperature w stopniach celciusza');
   
   temp = parseFloat(temp);
   
-  tempx = temp * 1.8 + 32;
+  var tempFahrenheit = temp * 1.8 + 32;
   
-  output.innerHTML = 'temperatura w stopniach celsjusza ' +temp;
+  output.innerHTML = 'temperatura w stopniach celsjusza ' +temp+ ' temperatura w stopniach farenchaita '+ tempFahrenheit;
   
-   if (isNaN(temp)) {
+  temperature(temp);
+  addNewLine();
+  waterOfStateCondition(temp);
+ 
+});
+
+buttonFahrenheit.addEventListener('click', function(){  
+    
+  var temp = window.prompt('wpisz temperature w stopniach celciusza');
+  
+  temp = parseFloat(temp);
+  
+  var tempCelsius = (temp - 32) / 1.8;
+  
+  output.innerHTML = 'temperatura w stopniach fahrenheita ' +temp+ ' temperatura w stopniach celsjusza '+ tempCelsius;  
+  
+  temperature(tempCelsius);
+  addNewLine();
+  waterOfStateCondition(tempCelsius);
+ 
+});
+
+function temperature (temp) {
+  if (temp !== temp) {
     window.prompt('nie podałeś liczby');
-    return 0;
   }
   
-  else if (temp < 0) {
-    output_2.innerHTML = 'brrr zimno';
+ else if (temp < 0) {
+    outputText.innerHTML = 'brrr zimno';
   }
   
   else if (temp === 0){
-   output_2.innerHTML = 'chyba woda zamarzła';
+   outputText.innerHTML = 'chyba woda zamarzła';
   }
   
-  else if (18 > temp && temp > 0 ) {
-    output_2.innerHTML ='nie za ciepło';
+  else if (18 >= temp && temp > 0 ) {
+    outputText.innerHTML ='nie za ciepło';
   }
   
-  else if ( 30 > temp && temp > 18) {
-    output_2.innerHTML ='mmm przyjemnie ciepło';
-  }
-  
-  else {
-    output_2.innerHTML ='za gorąco';
-  }
-  
-  waterOfStateCondition(); 
-});
-
-addNewLine();
-
-button_2.addEventListener('click', function(){  
-    
-  temp = window.prompt('wpisz temperature w stopniach celciusza');  
-  
-  tempx = (temp - 32) / 1.8;
-  
-  output.innerHTML = 'temperatura w stopniach fahrenheita ' +temp;
-  
-  if (isNaN(temp)) {
-    window.prompt('nie podałeś liczby');
-  }
-  
-  else if (tempx < 0) {
-    output_2.innerHTML = 'brrr zimno';
-  }
-  
-  else if (tempx === 0){
-   output_2.innerHTML = 'chyba woda zamarzła';
-  }
-  
-  else if (18 > tempx && tempx > 0 ) {
-    output_2.innerHTML ='nie za ciepło';
-  }
-  
-  else if ( 30 > tempx && tempx > 18) {
-    output_2.innerHTML ='mmm przyjemnie ciepło';
+  else if ( 30 >= temp && temp > 18) {
+    outputText.innerHTML ='mmm przyjemnie ciepło';
   }
   
   else {
-    output_2.innerHTML ='za gorąco';
+    outputText.innerHTML ='za gorąco';
   }
-  
-  waterOfStateCondition(); 
-});
+}
 
-addNewLine();
-
-function waterOfStateCondition() {
-  if (temp < 0 || tempx < 0) {
-    output_3.innerHTML ='woda zamarzła';
+function waterOfStateCondition(temp) {
+  if (temp < 0) {
+    waterState.innerHTML ='woda zamarzła';
   }
   
-  else if (temp === 0 || tempx === 0) {
-    output_3.innerHTML ='trzeba sprawdzić ;)';
+  else if (temp === 0) {
+    waterState.innerHTML ='trzeba sprawdzić';
   }
   
-  else if (temp > 0 && temp < 100 || tempx > 0 && tempx < 100) {
-    output_3.innerHTML ='woda w stanie ciekłym';
+  else if (temp > 0 && temp < 100) {
+    waterState.innerHTML ='woda w stanie ciekłym';
   }
   
   else {
-    output_3.innerHTML ='woda w stanie gazowym';
+    waterState.innerHTML ='woda w stanie gazowym';
   }
 }
 
 function addNewLine() { 
-    document.write('<br> New Line <br>');  
+  var newParagraph = document.createElement("P");                       
+  var text = document.createTextNode("nowa linia");    
+  newParagraph.appendChild(text);                                          
+  document.body.appendChild(newParagraph);
 }
